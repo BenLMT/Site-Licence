34d6a3eff50f8d48e6bb27594d22f866cbaf18e8
BenLMT <benoit.lematelot@gmail.com>
2017-10-25 14:46:59 +0200

    Navbar-include

---
+++ include/navbar.php
@@ -0,0 +1,27 @@
+<div class="navbar navbar-inverse navbar-fixed-top headroom" >
+		<div class="container">
+			<div class="navbar-header">
+				<!-- Button for smallest screens -->
+				<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse"><span class="icon-bar"></span> <span class="icon-bar"></span> <span class="icon-bar"></span> </button>
+				<a class="logo" href="index.html"><img src="assets/images/Logo final PNG.png" alt="Logo"></a>
+			</div>
+			<div class="navbar-collapse collapse">
+				<ul class="nav navbar-nav pull-right">
+					<li class="active"><a href="#">Accueil</a></li>
+					<li class="dropdown">
+						<a href="formation.html" class="dropdown-toggle" data-toggle="dropdown">Formation<b class="caret"></b></a>
+						<ul class="dropdown-menu">
+							<li><a href="sidebar-right.html">Programme</a></li>
+							<li><a href="sidebar-left.html">Équipements</a></li>
+							<li><a href="sidebar-left.html">Enseignants</a></li>
+							<li><a href="sidebar-left.html">Admission</a></li>
+							<li><a href="sidebar-left.html">Débouchés</a></li>
+						</ul>
+					</li>
+					<li><a href="galerie.html">Galerie</a></li>
+					<li><a href="contact.html">Contact</a></li>
+					<li><a class="btn" href="postuler.html">POSTULER</a></li>
+				</ul>
+			</div><!--/.nav-collapse -->
+		</div>
+	</div> 
